c9d5649440f8d3ecbc8ae78168c4d33e0800220d
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-04-11 13:57:11 +0200

    style(login): The login page styles has been modified. A new input component has been created

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,46 +2,51 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <h1 class="text-4xl text-center">
+        Iniciar sesión
+    </h1>
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <div class="input-wrapper mt-4">
+            <input type="email" id="email" name="email" :value="old('email')" required autofocus
+                autocomplete="username">
+            <label for="myInput" for="email">Correo electrónico</label>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="input-wrapper mt-4">
+            <input type="password" id="password" name="password" required autocomplete="current-password">
+            <label for="myInput" for="password">Contraseña</label>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox"
+                    class="rounded"
+                    name="remember">
+                <span class="ms-2 color-cBackground text-lg">{{ ('Recuérdame') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="block mt-4 grid grid-cols-2">
+            <a class="underline text-sm color-cText rounded-md" href="{{ route('register') }}">
+                {{ __('¿No tienes cuenta?') }}
+            </a>
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a class="underline text-sm color-cText rounded-md text-end" href="{{ route('password.request') }}">
+                    {{ __('Has olvidado tu contraseña?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <button type="submit" class="bg-cAccent color-cPrimary text-xl py-2 w-full">
+                Iniciar Sesión
+            </button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>  
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased bg-cBackground">
-        @include('layouts.navigation')
-        <div class="min-h-screen ">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        @include('layouts.footer')
-    </body>
+<body class="font-sans antialiased bg-cBackground">
+    @include('layouts.navigation')
+    <div class="min-h-screen ">
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    @include('layouts.footer')
+</body>
+
 </html>
