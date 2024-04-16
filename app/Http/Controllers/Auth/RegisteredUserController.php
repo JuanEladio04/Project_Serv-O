971bbfedc93ffb36e8caddc40173abdddecf5187
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-04-16 13:35:10 +0200

    fix(Register): An error with the register has been fixed The try catch has been fixed

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,35 +29,31 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        try {
-            $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'last_name' => ['required', 'string', 'max:255'],
-                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-                'password' => ['required', 'confirmed', Rules\Password::defaults()],
-                'phone_number' => ['nullable', 'string', 'digits:9', 'unique:'.User::class],
-                'birth_date' => ['date'],
-            ]);
-    
-            $user = User::create([
-    
-                'name' => $request->name,
-                'last_name' => $request->last_name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'phone_number' => $request->phone_number,
-                'birth_date' => $request->birth_date
-            ]);
-    
-            event(new Registered($user));
-    
-            Auth::login($user);
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone_number' => ['nullable', 'string', 'digits:9', 'unique:' . User::class],
+            'birth_date' => ['date'],
+        ]);
 
-            session()->flash('status','Usuario registrado correctamente');
-            return redirect(route('index', absolute: false));
-        } catch (\Throwable $th) {
-            session()->flash('status','No ha sido posible registrar al usuario');
-            return redirect(route('index'));
-        }
+        $user = User::create([
+
+            'name' => $request->name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone_number' => $request->phone_number,
+            'birth_date' => $request->birth_date
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        session()->flash('status', 'Usuario registrado correctamente');
+        return redirect(route('index', absolute: false));
+
     }
 }
