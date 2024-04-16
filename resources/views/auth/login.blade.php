d46b55751c5dae1dfca060354cda05c03f733107
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-04-16 17:16:10 +0200

    refactor(auth): The inputs has been change to the new CustomInputs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,33 +11,9 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <!-- Email Address -->
-                <div class="input-wrapper mt-4">
-                    <div class="relative z-0">
-                        <input type="email" id="email" name="email" value="{{ old('email') }}"
-                            class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                            placeholder=" " />
-                        <label for="email"
-                            class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            Correo electrónico
-                        </label>
-                    </div>
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                </div>
+                <x-CustomInput label="Correo electrónico" name="email" type="email" value="{{ old('email') }}"/>
 
-                <!-- Password -->
-                <div class="input-wrapper mt-4">
-                    <div class="relative z-0">
-                        <input type="password" id="password" name="password" required autocomplete="current-password"
-                            class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                            placeholder=" " />
-                        <label for="password"
-                            class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            Contraseña
-                        </label>
-                    </div>
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
+                <x-CustomInput label="Contraseña" name="password" type="password"/>
 
                 <!-- Remember Me -->
                 <div class="block mt-4">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,107 +10,24 @@
                 @csrf
 
                 <div class="grid sm:grid-cols-2 sm:gap-5">
-                    <!-- Name -->
-                    <div class="input-wrapper mt-6">
-                        <div class="relative z-0">
-                            <input type="text" id="name" name="name" value="{{ old('name') }}" autofocus
-                                class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                                placeholder=" " />
-                            <label for="name"
-                                class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                                Nombre
-                            </label>
-                        </div>
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                    </div>
+                    <x-CustomInput label="Nombre" name="name" type="text" value="{{ old('name') }}" />
 
-                    <!-- LastName -->
-                    <div class="input-wrapper mt-6">
-                        <div class="relative z-0">
-                            <input type="text" id="last_name" name="last_name" value="{{ old('last_name') }}"
-                                class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                                placeholder=" " />
-                            <label for="last_name"
-                                class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                                Apellidos
-                            </label>
-                        </div>
-                        <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
-                    </div>
+                    <x-CustomInput label="Apellidos" name="last_name" type="text" value="{{ old('last_name') }}" />
                 </div>
 
-                <!-- Email Address -->
-                <div class="input-wrapper mt-4">
-                    <div class="relative z-0">
-                        <input type="email" id="email" name="email" value="{{ old('email') }}"
-                            class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                            placeholder=" " />
-                        <label for="email"
-                            class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            Correo electrónico
-                        </label>
-                    </div>
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                </div>
+                <x-CustomInput label="Correo electrónico" name="email" type="email" value="{{ old('email') }}" />
 
+                <x-CustomInput label="Contraseña" name="password" type="password" />
 
-                <!-- Password -->
-                <div class="input-wrapper mt-4">
-                    <div class="relative z-0">
-                        <input type="password" id="password" name="password"
-                            class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                            placeholder=" " />
-                        <label for="password"
-                            class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            Contraseña
-                        </label>
-                    </div>
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
-
-                <div class="input-wrapper mt-6">
-                    <div class="relative z-0">
-                        <input type="password" id="password_confirmation" name="password_confirmation"
-                            class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                            placeholder=" " />
-                        <label for="password_confirmation"
-                            class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            Repetir contraseña
-                        </label>
-                    </div>
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                </div>
+                <x-CustomInput label="Repetir contrseña" name="password_confirmation" type="password" />
 
                 <div class="grid sm:grid-cols-2 gap-5 items-baseline">
-                    <div class="input-wrapper mt-6">
-                        <div class="relative z-0">
-                            <input type="number" id="phone_number" name="phone_number"
-                                value="{{ old('phone_number') }}"
-                                class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                                placeholder=" " />
-                            <label for="phone_number"
-                                class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                                Número de teléfono
-                            </label>
-                        </div>
-                        <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
-                    </div>
 
-                    <!-- birth_date -->
-                    <div class="input-wrapper mt-6">
-                        <div class="input-wrapper mt-6">
-                            <div class="relative z-0">
-                                <input type="date" id="birth_date" name="birth_date" value="{{ old('birth_date') }}"
-                                    class="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer"
-                                    placeholder=" " />
-                                <label for="birth_date"
-                                    class="absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                                    Fecha de nacimiento
-                                </label>
-                            </div>
-                            <x-input-error :messages="$errors->get('birth_date')" class="mt-2" />
-                        </div>
-                    </div>
+                    <x-CustomInput label="Numero de teléfono" name="phone_number" type="number"
+                        value="{{ old('phone_number') }}" />
+
+                    <x-CustomInput label="Fecha de nacimiento" name="birth_date" type="date"
+                        value="{{ old('birth_date') }}" />
                 </div>
 
                 <div class="flex items-center justify-center mt-6">
