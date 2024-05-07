873e15b2c828660fe59a6b8c74f15a062a58f572
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-07 18:26:22 +0200

    style(ForgottenPassword): The styles from the forgotten password page has been readjusted

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,28 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="sm:w-1/3 w-full p-0 m-5">
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+        <x-customUserForm>
+            <div class="mb-4 text-sm color-ctext text-center">
+                {{ '¿Olvidaste tu contraseña? No hay problema. Simplemente déjanos saber tu dirección de correo electrónico y te enviaremos un enlace de restablecimiento de contraseña por correo electrónico que te permitirá elegir una nueva.' }}
+            </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+                <!-- Email Address -->
+                <x-CustomInput label="Correo electrónico" name="email" type="email" value="{{ old('email') }}" />
+
+                <div class="flex items-center justify-end mt-4">
+                    <button type="submit" class="accentButton cSubTitle mt-10">
+                        Recuperar contraseña
+                    </button>
+                </div>
+            </form>
+        </x-customUserForm>
+    </div>
+
 </x-guest-layout>
