ad05225cab6b6f51225fae3ef8ef173cffce0396
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-07 13:31:01 +0200

    style: Some modals size has been readjusted

--- resources/views/livewire/create-server-modal.blade.php
+++ resources/views/livewire/create-server-modal.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div id="insertServerModal" tabindex="-1"
         class="{{ $showModal ? 'flex' : 'hidden' }} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
             <div class="relative bg-cPrimary rounded-2xl shadow text-center">
                 <div class="w-full text-right pt-10 pr-10 m-none">
                     <button class="color-cBackground" data-modal-target="insertServerModal"
