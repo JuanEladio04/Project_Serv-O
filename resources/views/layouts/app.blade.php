2db0c0a15ad40cb46e06bea92cc0aba8a0740660
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-08 12:49:59 +0200

    feat(index): The index page has been created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,16 +19,6 @@
 <body class="font-sans antialiased bg-cBackground">
     @include('layouts.navigation')
     <div class="min-h-screen ">
-
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-
         <!-- Page Content -->
         <main>
             {{ $slot }}
