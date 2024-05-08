a3f139993789faa7b7c3151547d6d3709bdc8ddb
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-08 20:03:14 +0200

    style(footer): The footer styles has been fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,15 @@
-<footer class="footer bg-cPrimary color-cText p-4 justify-center text-center grid grid-cols-3 mt-10">
-    <div>
+<footer class="footer bg-cPrimary color-cText p-4 justify-center text-center grid sm:grid-cols-3 grid-cols-1 mt-10">
+    <div class="order-2 sm:order-1 sm:text-xl text-2xl my-5">
         <a class="cSubTitle block" href="">Terminos y condiciones</a>
         <a class="cSubTitle block" href="">Política de coockies</a>
         <a class="cSubTitle block" href="">Política de privacidad</a>
     </div>
-    <div class="flex justify-center items-center"> <!-- Agrega las clases "flex justify-center items-center" -->
+    <div class="flex justify-center items-center order-1 sm:order-2 my-5"> 
         <a href="{{ route('index') }}">
             <x-application-logo class="block h-9 w-auto fill-current color-cPrimary">w-32</x-application-logo>
         </a>
     </div>
-    <div>
+    <div class="border-3 order-3 sm:text-xl text-2xl my-5">
         <p class="cSubTitle block">Contactanos:</p>
         <p class="cSubTitle block">X@correo.com</p>
         <p class="cSubTitle block">+34 000 00 00 00</p>
