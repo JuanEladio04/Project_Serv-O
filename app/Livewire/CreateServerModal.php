8410b1d357fd52fa3e671c26038d5fe98d820351
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-09 10:09:28 +0200

    feat(WorkSpace): Some validations has been added

--- app/Livewire/CreateServerModal.php
+++ app/Livewire/CreateServerModal.php
@@ -24,7 +24,7 @@ class CreateServerModal extends Component
     }
 
     public function performInsert()
-    {        
+    {
         $this->showModal = true;
         $this->validate([
             'name' => 'required|string|max:255',
@@ -42,20 +42,59 @@ class CreateServerModal extends Component
             $server->user_root = $this->user;
             $encryptionHelper = new EncryptionHelper();
             $server->password = $encryptionHelper->encryptPassword($this->password, env('ENCRYPTION_KEY'));
-            $server->workSpace()->associate($this->workSpace->id);
-            $server->save();
 
-            $this->dispatch('newServerAdded');
-            $this->statusMessage = 'Servidor añadido correctamente.';
-            $this->resetInputFields();
+            if ($this->checkAvailability($server)) {
+                $server->workSpace()->associate($this->workSpace->id);
+                $server->save();
+
+                $this->dispatch('newServerAdded');
+                $this->statusMessage = 'Servidor añadido correctamente.';
+                $this->resetInputFields();
+            }
         } catch (\Throwable $th) {
             $this->statusMessage = 'No ha sido posible añadir servidor. ' . $th;
         }
     }
 
+    /**
+     * Reset the input fields of the form.
+     *
+     * This method is used to clear the input fields of the form after a successful server insertion.
+     * It resets the 'name', 'description', 'direction', 'password', and 'user' properties of the component.
+     *
+     * @return void
+     */
     private function resetInputFields()
     {
         $this->reset(['name', 'description', 'direction', 'password', 'user']);
     }
 
+    /**
+     * Check if a server is available to be added.
+     *
+     * This method checks if a server with the same direction or already exists in the workspace.
+     * If the server is not available, it sets an appropriate status message and resets the input fields.
+     *
+     * @param Server $server The server to be checked.
+     * @return boolean
+     */
+    private function checkAvailability(Server $server)
+    {
+        if ($server->server_dir == 'localhost' || $server->server_dir == '127.0.0.1') {
+            $this->statusMessage = 'No se puede añadir un servidor con dirección localhost o 127.0.0.1';
+            $this->resetInputFields();
+            return false;
+        } else {
+            foreach ($this->workSpace->servers as $currentServer) {
+                if ($currentServer->server_dir == $server->server_dir) {
+                    $this->statusMessage = 'Ese servidor ya ha sido añadido';
+                    $this->resetInputFields();
+                    return false;
+                }
+            }
+        }
+    }
+
 }
+
+
