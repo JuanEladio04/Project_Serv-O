4faa0528ca89dd2e305edfea4316da8bc5bae9c6
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-15 13:54:34 +0200

    feat(WebSocket): reverb has been replaced by pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,21 +11,5 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 import "./echo";
 
-import Echo from "laravel-echo";
 
-import Pusher from "pusher-js";
-window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: "reverb",
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
-    enabledTransports: ["ws", "wss"],
-});
-
-window.Echo.channel("ping").listen(".PingPerformed", (e) => {
-    console.log("Resultado del ping: ", e.result);
-});
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -2,13 +2,14 @@ import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
-
+ 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true
+});
+
+window.Echo.channel("ping").listen("PingPerformed", (e) => {
+    console.log("Resultado del ping: ", e.result);
 });
