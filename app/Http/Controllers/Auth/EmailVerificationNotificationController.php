337401c87e44d7c294474bd1a59798ada8290d44
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-16 13:53:56 +0200

    fix(emailVerification): An issue has been fixed

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\Controller;
@@ -20,18 +19,18 @@ class EmailVerificationNotificationController extends Controller
             return redirect()->intended(route('index', [], false));
         }
 
-        $user = $request->user();
-        $hash = Str::random(32); // Genera un hash aleatorio
-
-        VerifyEmail::createUrlUsing(function ($notifiable) use ($user, $hash) {
+        VerifyEmail::createUrlUsing(function ($notifiable) use ($request) {
             return URL::temporarySignedRoute(
                 'verification.verify',
                 now()->addMinutes(60),
-                ['id' => $user->getKey(), 'hash' => $hash] 
+                [
+                    'id' => $request->user()->getKey(),
+                    'hash' => sha1($request->user()->getEmailForVerification()),
+                ]
             );
         });
 
-        $user->sendEmailVerificationNotification();
+        $request->user()->notify(new VerifyEmail);
 
         return back()->with('verificationStatus', 'verification-link-sent');
     }
