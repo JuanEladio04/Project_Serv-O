7e328a468f103e5c3c391e54b1927bb3d41372db
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-17 12:15:10 +0200

    feat(errors): Dynamic custom error views has been added.

---
+++ resources/views/errors/error.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+
+    <h1 class="text-7xl cTitle my-10">Error <span class="color-cAccent">{{ $statusCode }}</span></h1>
+    <p class="text-5xl">{{ $errorMessage }}</p>
+
+</x-guest-layout>
