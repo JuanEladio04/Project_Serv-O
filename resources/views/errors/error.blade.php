a7054a6d21de03affaf7b5c0e39d4ffa9e9ded68
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-17 12:23:44 +0200

    style(error): some styles has been readjusted

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -1,6 +1,11 @@
-<x-guest-layout>
+<x-app-layout>
 
-    <h1 class="text-7xl cTitle my-10">Error <span class="color-cAccent">{{ $statusCode }}</span></h1>
-    <p class="text-5xl">{{ $errorMessage }}</p>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <a href="/" class="block">
+            <x-application-logo class="block h-9 w-auto fill-current color-cPrimary">w-48</x-application-logo>
+        </a>
+        <h1 class="text-7xl cTitle my-10">Error <span class="color-cAccent">{{ $statusCode }}</span></h1>
+        <p class="text-5xl">{{ $errorMessage }}</p>
+    </div>
 
-</x-guest-layout>
+</x-app-layout>
