40c2e16962f61f5171e0737d6ebc9d26842513c6
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-05-29 13:47:15 +0200

    Revert "HOTFIX: A mistake with email verification has been fixed"
    This reverts commit c160606d44693d636f8fcd4b8c572ef242816982.

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -14,11 +16,22 @@ class EmailVerificationNotificationController extends Controller
     public function store(Request $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('index', absolute: false));
+            return redirect()->intended(route('index', [], false));
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        VerifyEmail::createUrlUsing(function ($notifiable) use ($request) {
+            return URL::temporarySignedRoute(
+                'verification.verify',
+                now()->addMinutes(60),
+                [
+                    'id' => $request->user()->getKey(),
+                    'hash' => sha1($request->user()->getEmailForVerification()),
+                ]
+            );
+        });
 
-        return back()->with('status', 'verification-link-sent');
+        $request->user()->notify(new VerifyEmail);
+
+        return back()->with('verificationStatus', 'verification-link-sent');
     }
 }
